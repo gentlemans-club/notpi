41cb69c42b1016a2d1523e6a88dfa21acc252525
Kent <kentsd16@student.uia.no>
2019-01-20 17:52:11 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='notpi',
-    version='0.0.1',
+    version='0.1.0',
     author='gentlemans-club',
     author_email='kentsd16@student.uia.no',
     packages=['notpi'],
